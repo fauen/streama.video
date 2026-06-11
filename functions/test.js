db57a29c6ac6d34265b353aa0f74afa110ceb1fe
Daniel Bäckman <daniel@backman.io>
2026-06-11 07:41:23 +0200

    Remove test function

--- functions/test.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ message: 'Test function works!' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-}
